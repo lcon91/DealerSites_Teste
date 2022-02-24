f35ca167c52442703804c0945bfbfb8373dd78f3
Luiz Costa <luizcostaon@gmail.com>
2022-02-24 20:14:51 -0300

    Ajuste para recurso de login

--- base/Application.php
+++ base/Application.php
@@ -12,18 +12,20 @@ class Application{
     public function run(){
         session_start();
 
+        $params = $_GET;
+
         if(!isset($_SESSION['dealersite_user_id'])){
             $controller = new Login;
             $method = $params['method'] ?? null;
             
-            $content = $controller;
             if($method){
                 $controller->$method();
             }
 
+            $content = $controller;
+
             $this->template->renderLogin($content);
         }else{
-            $params = $_GET;
             $class = $params['class'] ?? 'Home';
             $method = $params['method'] ?? null;
             
@@ -37,7 +39,5 @@ class Application{
 
             $this->template->render($content);
         }
-
-        
     }
 }
